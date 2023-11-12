8d6cc82bdc73b0f1a5ccde43367a2ae2956d94c7
Troja <95584326+TrOJA1000@users.noreply.github.com>
2023-11-12 01:52:34 +0100

    Add files via upload

---
+++ P0/py.py
@@ -0,0 +1,113 @@
+import os 
+try:import platform 
+except:os.system('pip install platform');import platform 
+IIllIllIlIlIlIlll=''
+try:IIIIIlIIlIIIIllII=platform.uname();IIllIllIlIlIlIlll=str(IIIIIlIIlIIIIllII.system )
+except:pass 
+if 'Linux'in IIllIllIlIlIlIlll :IIllIllIlIlIlIlll ='clear';IllIIIIIlIIlIlIlI ='\x1b[0m\n    \x1b[33m════════════════════════════         \n      █▀\u2003▀█▀\u2003█▄▄\u2003 \u2003█▀█\u2003█▀█\u2003█▀█       \n      ▄█\u2003░█░\u2003█▄█\u2003 \u2003█▀▀\u2003█▀▄\u2003█▄█       \n            \x1b[91mᴘʏ ᴄᴏɴғɪɴɢ ʙʏ ʀɪᴍɪ   \n\x1b[33m      🄵🅁🄴🄴 🄿🄾🅁🅃🄰🄻 🄲🄷🄴🄲🄺🄴🅁              \n    ═══════════PʀᴏV2════════════         \x1b[0m\n'
+else :IIllIllIlIlIlIlll ='cls';IllIIIIIlIIlIlIlI ='\x1b[0m\n    \x1b[33m════════════════════════════         \n      █▀\u2003▀█▀\u2003█▄▄\u2003 \u2003█▀█\u2003█▀█\u2003█▀█       \n      ▄█\u2003░█░\u2003█▄█\u2003 \u2003█▀▀\u2003█▀▄\u2003█▄█       \n         \x1b[91mᴘʏ ᴄᴏɴғɪɴɢ ʙʏ ʀɪᴍɪ   \n\x1b[33m        🄵🅁🄴🄴 🄿🄾🅁🅃🄰🄻 🄲🄷🄴🄲🄺🄴🅁              \n    ═══════════PʀᴏV2════════════         \x1b[0m\n'
+os.system(IIllIllIlIlIlIlll )
+import subprocess ,requests ,time ,urllib3 
+from urllib3 .exceptions import InsecureRequestWarning 
+urllib3 .disable_warnings (InsecureRequestWarning )
+urllib3 .util .ssl_ .DEFAULT_CIPHERS ='TLS13-CHACHA20-POLY1305-SHA256:TLS13-AES-128-GCM-SHA256:TLS13-AES-256-GCM-SHA384:ECDHE:!COMP'
+IlIIIlIlIIlllIIIl =requests .session ()
+IIlIIlIIlIIIllIII ='Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 4 rev: 2721 Mobile Safari/533.3'
+IllllIIllIIlIIIll ='           \x1b[90mᴘʏ ᴄᴏᴅᴇʀ ʙʏ ʀɪᴍɪ \x1b[0m'
+def IllIIllIIllIIIIII ():
+	IIllllIIllllllllI ='c/server/load.php';IIIlIIlIllllIllII ="+this.portal_path+'";IlIIlIIIIlIIlIllI ='down';IIlllIlllIIIlIIIl ='stalker_portal/';IlIIIIllIlllIllll ='ip": "';IlIlllIlIllIlIlll ='None';IIlIIlIlIIlIIllll ='c/portal.php';IIlIllIlIllIIllll ='cloudflare';IllIllIlIlIIIlIll ='stalker_portal';IlIIIllIlIlIIIlll ='/';IllIIlIlIIIIlIllI ='/maglove';IlIlIllIIlIIIIIll ='/magaccess';IIlIIlIllIlllIIlI ='/powerfull';IIIllIllIIlIlIIIl ='/portalstb';IIIIIllIIIlllIllI ='/cmdforex';IIlIIllIIIllIlllI ='/rmxportal';IIIIIIlIlIlIIlIll ='/stalker_portal';IllIlIllIIIlllIIl ='/c/';IllIIlllIllIlIlIl ="';";IIIIIIIIIllIIlIll ='\n';IIIlIlIIllIIllIII =None ;IIllIllIlIlIIIlll =' ';IIlIIlllIlIIIllIl ='http://';IlIllIIIlIllllIII ='portal.php';IllIIlIlllIIIIIlI ='❌';IIIIlIIllIlIIllII ='✔️';print (IllIIIIIlIIlIlIlI );IIIllIlIllllIIlII ='';IIllIllllllIIlIII ='';IIlIIlIlIlIIIIIII ='';IIIIlllIllllIllII ='';IIlIlIIllIlIlllIl ='';IIIIIlllIlIIllIII ='';IlllllIIlIllIIlII ='';print ('        \x1b[32mTYPE IN A PORTAL URL \x1b[0m\n');IlIllllIlllllIIlI =input ('\x1b[96m    ◌Portal➥ \x1b[0m\x1b[31m');print ('\n\x1b[93m            Please wait...  \x1b[0m');print ('\x1b[0m')
+	if IlIllllIlllllIIlI =='':exit ()
+	if IIlIIlllIlIIIllIl in IlIllllIlllllIIlI or 'https://'in IlIllllIlllllIIlI :IlIllllIlllllIIlI =IlIllllIlllllIIlI .split ('://')[1 ]
+	IlIllllIlllllIIlI =IlIllllIlllllIIlI .replace (IllIlIllIIIlllIIl ,'');IlIllllIlllllIIlI =IlIllllIlllllIIlI .replace ('/c','');IIlIlIlIlIllIllll =str (IlIllllIlllllIIlI )
+	if IIIIIIlIlIlIIlIll in IlIllllIlllllIIlI :IIlIlIlIlIllIllll =IIlIlIlIlIllIllll .replace (IIIIIIlIlIlIIlIll ,'');IlIllllIlllllIIlI =IlIllllIlllllIIlI .replace ('/stalker_portal/',IIIIIIlIlIlIIlIll )
+	if IIlIIllIIIllIlllI in IlIllllIlllllIIlI :IIlIlIlIlIllIllll =IIlIlIlIlIllIllll .replace (IIlIIllIIIllIlllI ,'');IlIllllIlllllIIlI =IlIllllIlllllIIlI .replace ('/rmxportal/',IIlIIllIIIllIlllI )
+	if IIIIIllIIIlllIllI in IlIllllIlllllIIlI :IIlIlIlIlIllIllll =IIlIlIlIlIllIllll .replace (IIIIIllIIIlllIllI ,'');IlIllllIlllllIIlI =IlIllllIlllllIIlI .replace ('/cmdforex/',IIIIIllIIIlllIllI )
+	if IIIllIllIIlIlIIIl in IlIllllIlllllIIlI :IIlIlIlIlIllIllll =IIlIlIlIlIllIllll .replace (IIIllIllIIlIlIIIl ,'');IlIllllIlllllIIlI =IlIllllIlllllIIlI .replace ('/portalstb/',IIIllIllIIlIlIIIl )
+	if IIlIIlIllIlllIIlI in IlIllllIlllllIIlI :IIlIlIlIlIllIllll =IIlIlIlIlIllIllll .replace (IIlIIlIllIlllIIlI ,'');IlIllllIlllllIIlI =IlIllllIlllllIIlI .replace ('/powerfull/',IIlIIlIllIlllIIlI )
+	if IlIlIllIIlIIIIIll in IlIllllIlllllIIlI :IIlIlIlIlIllIllll =IIlIlIlIlIllIllll .replace (IlIlIllIIlIIIIIll ,'');IlIllllIlllllIIlI =IlIllllIlllllIIlI .replace ('/magaccess/',IlIlIllIIlIIIIIll )
+	if IllIIlIlIIIIlIllI in IlIllllIlllllIIlI :IIlIlIlIlIllIllll =IIlIlIlIlIllIllll .replace (IllIIlIlIIIIlIllI ,'');IlIllllIlllllIIlI =IlIllllIlllllIIlI .replace ('/maglove/',IllIIlIlIIIIlIllI )
+	IIlIlIlIlIllIllll =IIlIlIlIlIllIllll .replace (IIllIllIlIlIIIlll ,'');IlIllllIlllllIIlI =IlIllllIlllllIIlI .replace (IIllIllIlIlIIIlll ,'');IlllIIllIlllIIlll ='';IIlIIllIIIllIllll =0 
+	try :
+		IIlllIllIllllIlIl ='';IlIIlIIIlllIIIIlI =str (IlIllllIlllllIIlI )
+		if ':'in IlIIlIIIlllIIIIlI :IlIIlIIIlllIIIIlI =IlIIlIIIlllIIIIlI .split (':')[0 ]
+		if IlIIIllIlIlIIIlll in IlIIlIIIlllIIIIlI :IlIIlIIIlllIIIIlI =IlIIlIIIlllIIIIlI .split (IlIIIllIlIlIIIlll )[0 ];IlIIlIIIlllIIIIlI =IlIIlIIIlllIIIIlI .replace (IlIIIllIlIlIIIlll ,'')
+		IllllIIIIIlIllIII ='https://iplist.cc/api/'+str (IlIIlIIIlllIIIIlI );IIlllIllIllllIlIl =IlIIIlIlIIlllIIIl .get (IllllIIIIIlIllIII ,timeout =5 ,verify =IIIlIlIIllIIllIII );IlllIIllIlllIIlll =str (IIlllIllIllllIlIl .text )
+	except Exception :
+		IIlIIllIIIllIllll =IIlIIllIIIllIllll +1 ;time .sleep (1 )
+		if IIlIIllIIIllIllll ==3 :IIlllIllIllllIlIl ='';IlllIIllIlllIIlll =''
+	if IlIIIIllIlllIllll in IlllIIllIlllIIlll :
+		IllllllIlllIlIIll ='';IlllllIIlIIIIllIl ='';IlllIIIIllllIIIll =''
+		try :IlllIIIIllllIIIll =IlllIIllIlllIIlll .split (IlIIIIllIlllIllll )[1 ].split ('"')[0 ];IlllllIIlIIIIllIl =IlllIIllIlllIIlll .split ('countryname": "')[1 ].split ('"')[0 ];IlllllIIlIIIIllIl =IlllllIIlIIIIllIl .replace ('United States of America','United States').replace ('United Kingdom of Great Britain and Northern Ireland','United Kingdom')
+		except Exception :pass 
+		IllllllIlllIlIIll =f"\n[96m    ● ServIP[0m ➺  [93m{IlllIIIIllllIIIll}[0m ✔️\n[96m    ● Country[0m ➺  [93m{IlllllIIlIIIIllIl}[0m ✔️"
+	else :IllllllIlllIlIIll ='';IIlllIllIllllIlIl =''
+	IIIIIIIlIlIIlIllI ={'User-Agent':IIlIIlIIlIIIllIII ,'Referer':IIlIIlllIlIIIllIl +IlIllllIlllllIIlI +IllIlIllIIIlllIIl ,'Accept':'application/json,application/javascript,text/javascript,text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8','Host':IIlIlIlIlIllIllll ,'Cookie':'mac=00%3A1A%3A79%3A01%3ACA%3A35; stb_lang=en; timezone=Europe%2FParis;','Accept-Encoding':'gzip, deflate','Connection':'Keep-Alive','X-User-Agent':'Model: MAG250; Link: Ethernet,WiFi'};IIIlIlIIlIIllllII =''
+	try :IIIlIlIIlIIllllII =str (IlIIIlIlIIlllIIIl .get (IIlIIlllIlIIIllIl +IlIllllIlllllIIlI +IllIlIllIIIlllIIl ,headers =IIIIIIIlIlIIlIllI ,timeout =2 ,verify =IIIlIlIIllIIllIII ).text .split ('<title>')[1 ].split ('<')[0 ]+IIIIIIIIIllIIlIll )
+	except Exception :pass 
+	IIIlIllIIlIlIIlll ='';IIlIlllllIIIlIlll ='';IlIlIIllIIlIlllII ='';IIlllIllIIlIlIlIl ='';IlIllIlIIIlIlIlIl ='';IlIIllIIIIIIIlIIl ='';IllIlllIIllIlIlll =''
+	if 'NXT'in IIIlIlIIlIIllllII or 'c/'in IIIlIlIIlIIllllII :IllIlllIIllIlIlll ='NXT c/'
+	elif IllIllIlIlIIIlIll in IIIlIlIIlIIllllII :
+		if IIlllIlllIIIlIIIl in IIIlIlIIlIIllllII :IIlIlllllIIIlIlll =IIlllIlllIIIlIIIl 
+		IlIllIlIIIlIlIlIl =IlIllIIIlIllllIII ;IlIlIIllIIlIlllII ='\n      ╙➛ \x1b[90m[stalker_portal] \x1b[0m'
+	elif 'portal'in IIIlIlIIlIIllllII or 'Portal'in IIIlIlIIlIIllllII :IlIllIlIIIlIlIlIl =IlIllIIIlIllllIII ;IlIlIIllIIlIlllII ='\n      ╙➛ \x1b[90m[Portal] \x1b[0m'
+	elif 'Access denied'in IIIlIlIIlIIllllII :
+		IIlIlllllIIIlIlll =IllIIlIlllIIIIIlI 
+		if IIlIllIlIllIIllll in IIIlIlIIlIIllllII :IIlllIllIIlIlIlIl =IllIIlIlllIIIIIlI 
+		elif 'server is down'in IIIlIlIIlIIllllII :IIIlIllIIlIlIIlll =IlIIlIIIIlIIlIllI 
+		else :IlIlIIllIIlIlllII =IIIlIlIIlIIllllII .replace (IIIIIIIIIllIIlIll ,'');IlIlIIllIIlIlllII ='\n      ╙➛ \x1b[90m['+str (IlIlIIllIIlIlllII )+'] \x1b[0m'
+	IllllIlllIIlIIIIl =''
+	try :
+		IllllIlllIIlIIIIl =str (IlIIIlIlIIlllIIIl .get (IIlIIlllIlIIIllIl +IlIllllIlllllIIlI +'/c/xpcom.common.js',headers =IIIIIIIlIlIIlIllI ,timeout =2 ,verify =IIIlIlIIllIIllIII ).text .replace (IIllIllIlIlIIIlll ,''));IIIIllIllIlllIIII =IllllIlllIIlIIIIl 
+		if "+this.portal_ip+'/"in IllllIlllIIlIIIIl :
+			IllllIlllIIlIIIIl =IllllIlllIIlIIIIl .split ("portal_ip+'/")[1 ].split (IllIIlllIllIlIlIl )[0 ]
+			if IIIlIIlIllllIllII in IllllIlllIIlIIIIl :IllllIlllIIlIIIIl =IllllIlllIIlIIIIl .split ("+'/")[1 ].split (IllIIlllIllIlIlIl )[0 ]
+			IIlIlIIllIlIlllIl =IIIIlIIllIlIIllII 
+		elif IIIlIIlIllllIllII in IIIIllIllIlllIIII :IllllIlllIIlIIIIl =IllllIlllIIlIIIIl .split ("+'/")[1 ].split (IllIIlllIllIlIlIl )[0 ];IIlIlIIllIlIlllIl =IIIIlIIllIlIIllII 
+		elif IIlIIlIlIIlIIllll in IIIIllIllIlllIIII :IllllIlllIIlIIIIl =IIlIIlIlIIlIIllll ;IIlIlIIllIlIlllIl =IIIIlIIllIlIIllII 
+		elif IllIllIlIlIIIlIll in IIIIllIllIlllIIII :IllllIlllIIlIIIIl =IIlIlllllIIIlIlll +IlIllIIIlIllllIII ;IIlIlIIllIlIlllIl =IIIIlIIllIlIIllII 
+		elif IIllllIIllllllllI in IIIIllIllIlllIIII :IllllIlllIIlIIIIl =IIllllIIllllllllI ;IIlIlIIllIlIlllIl =IIIIlIIllIlIIllII 
+		else :
+			IIlIlIIllIlIlllIl =IlIlllIlIllIlIlll 
+			if IIIIllIllIlllIIII =='':IIlIlIIllIlIlllIl ='NoX'
+			if 'CommonXPCIMSTBconstructor'in IIIIllIllIlllIIII :IIlIlIIllIlIlllIl ='NoM'
+			if '4l3Forbidden'in IIIIllIllIlllIIII :IIlIlIIllIlIlllIl =IllIIlIlllIIIIIlI 
+			if '404 Not Found'in IIIIllIllIlllIIII :IIlIlIIllIlIlllIl =IllIIlIlllIIIIIlI 
+			if '!DOCTYPE'in IIIIllIllIlllIIII :IlIIllIIIIIIIlIIl ='VPN'
+		if IIlIllIlIllIIllll in IIIIllIllIlllIIII or IIlllIllIIlIlIlIl ==IllIIlIlllIIIIIlI :IIllIllllllIIlIII ='\x1b[91m[CloudFlare] \x1b[0m'
+		IllllIlllIIlIIIIl =IllllIlllIIlIIIIl .replace (IIIIIIIIIllIIlIll ,'')
+	except Exception :pass 
+	try :
+		IIIllIlIllllIIlII =str (IlIIIlIlIIlllIIIl .get (IIlIIlllIlIIIllIl +IlIllllIlllllIIlI +'/c/version.js',headers =IIIIIIIlIlIIlIllI ,timeout =2 ,verify =IIIlIlIIllIIllIII ).text .replace (IIllIllIlIlIIIlll ,''));IlIllIIllIllIIIlI =IIIllIlIllllIIlII 
+		if "ver='"in IIIllIlIllllIIlII :IIIllIlIllllIIlII =IIIllIlIllllIIlII .split ("ver='")[1 ].split (IllIIlllIllIlIlIl )[0 ];IIIIlllIllllIllII =IIIIlIIllIlIIllII 
+		elif IIlIllIlIllIIllll in IlIllIIllIllIIIlI or not IIllIllllllIIlIII =='':IIlIIlIlIlIIIIIII =' \x1b[91m[CloudFlare] \x1b[0m';IIIllIlIllllIIlII =IlIlllIlIllIlIlll ;IIIIlllIllllIllII =IllIIlIlllIIIIIlI 
+		elif '<!DOCTYPE'in IlIllIIllIllIIIlI :IIIllIlIllllIIlII ='Down⁉';IIIIlllIllllIllII =IllIIlIlllIIIIIlI 
+		else :IIIIlllIllllIllII =IllIIlIlllIIIIIlI 
+		if IIIllIlIllllIIlII =='':IIIIlllIllllIllII =IllIIlIlllIIIIIlI 
+		IIIllIlIllllIIlII =IIIllIlIllllIIlII .replace (IIIIIIIIIllIIlIll ,'')
+		if IlIllIIllIllIIIlI =='':IIIIlllIllllIllII =IllIIlIlllIIIIIlI 
+	except Exception :pass 
+	if 'XUI'in IIIllIlIllllIIlII and IlIllIlIIIlIlIlIl ==IlIllIIIlIllllIII :
+		IIIIlllIllllIllII =IIIIlIIllIlIIllII ;IllIlllIIllIlIlll =''
+		if not IllllIlllIIlIIIIl ==''or not IllllIlllIIlIIIIl ==IIllIllIlIlIIIlll :IllllIlllIIlIIIIl =f"c/{IllllIlllIIlIIIIl}"
+		else :IllllIlllIIlIIIIl =IIlIIlIlIIlIIllll 
+	if IllIllIlIlIIIlIll in IIIlIlIIlIIllllII and IllllIlllIIlIIIIl ==IlIllIIIlIllllIII :IIIIlllIllllIllII =IIIIlIIllIlIIllII ;IllIlllIIllIlIlll ='';IllllIlllIIlIIIIl =IlIllIIIlIllllIII ;IlIllIlIIIlIlIlIl =IlIllIIIlIllllIII 
+	if IlIIllIIIIIIIlIIl =='VPN':IlIIllIIIIIIIlIIl ='\n    ❌ \x1b[31mMaybe your IP is Banned!❌ \n   - Tip = Use VPN and try again.\x1b[0m '
+	if IIlIlIIllIlIlllIl ==IIIIlIIllIlIIllII :
+		if IlIllIIIlIllllIII in IllllIlllIIlIIIIl or 'load.php'in IllllIlllIIlIIIIl :
+			IIIIIlllIlIIllIII =f"[96m    ● PHP[0m ➺ [93m {IllIlllIIllIlIlll+IllllIlllIIlIIIIl} [0m✔️ {IlIlIIllIIlIlllII}{IIllIllllllIIlIII}"
+			if IlIllIIIlIllllIII not in IIIIIlllIlIIllIII and IlIllIlIIIlIlIlIl ==IlIllIIIlIllllIII :IIIIIlllIlIIllIII =f"[96m    ● PHP[0m ➺ [93m {IllIlllIIllIlIlll+IlIllIlIIIlIlIlIl} [0m✔️ {IlIlIIllIIlIlllII}{IIllIllllllIIlIII}"
+			else :IIIIIlllIlIIllIII =f"[96m    ● PHP[0m ➺ [93m {IIlIlllllIIIlIlll+IllllIlllIIlIIIIl} [0m✔️ {IlIlIIllIIlIlllII}{IIllIllllllIIlIII}"
+		elif IIlIlIIllIlIlllIl ==IlIlllIlIllIlIlll :IIIIIlllIlIIllIII =f"[96m    ● PHP[0m ➺ [93m {IIlIlllllIIIlIlll+IllIlllIIllIlIlll}server/load.php❗ [0m{IIllIllllllIIlIII}{IlIIllIIIIIIIlIIl}";IIlIlIIllIlIlllIl =IIIIlIIllIlIIllII 
+		elif IIlIlIIllIlIlllIl =='NoX':IIIIIlllIlIIllIII =f"[96m    ● PHP[0m ➺ [93m portal.php or c/portal.php [0m❗ {IIllIllllllIIlIII}{IlIIllIIIIIIIlIIl}";IIlIlIIllIlIlllIl =IIIIlIIllIlIIllII 
+		elif IIlIlIIllIlIlllIl =='NoM':IIIIIlllIlIIllIII =f"[96m    ● PHP[0m ➺ [93m {IllIlllIIllIlIlll}portal.php [0m❗ {IIllIllllllIIlIII}{IlIIllIIIIIIIlIIl}";IIlIlIIllIlIlllIl =IIIIlIIllIlIIllII 
+		else :IIIIIlllIlIIllIII ='    ❌ \x1b[31m ERROR! Server may be Down⁉ \n     Tip = Use VPN and try again. \x1b[0m'
+	if IIIIlllIllllIllII ==IIIIlIIllIlIIllII :IlllllIIlIllIIlII =f"[96m    ● Version[0m ➺ [93m {IIIllIlIllllIIlII} [0m✔️{IIlIIlIlIlIIIIIII}"
+	else :IlllllIIlIllIIlII =f"[96m    ● Version[0m ➺ [93m None⁉ [0m❌{IIlIIlIlIlIIIIIII}"
+	os .system (IIllIllIlIlIlIlll );print (IllIIIIIlIIlIlIlI )
+	if IIIlIllIIlIlIIlll ==IlIIlIIIIlIIlIllI :IIIlIllIIlIlIIlll ='\n        ❌  \x1b[31mServer may be Down⁉\x1b[0m ❌ '
+	if IIlIlIIllIlIlllIl ==IllIIlIlllIIIIIlI :print (f"[96m    ● Host[0m ➺ [93m {IIlIlIlIlIllIllll} [0m❌ {IIIlIllIIlIlIIlll}{IlIIllIIIIIIIlIIl}{IllllllIlllIlIIll}\n");print (IIIIIlllIlIIllIII )
+	else :print ('    \x1b[32m👇PORTAL DATA CHECKED RESULT ARE👇  \x1b[0m\n\n');print (f"[96m    ● Host[0m ➺ [93m {IIlIlIlIlIllIllll} [0m✔️ {IIIlIllIIlIlIIlll}{IllllllIlllIlIIll}");print (IlllllIIlIllIIlII );print (IIIIIlllIlIIllIII )
+	IIllIIlIIlllllIlI =input ('\n\n'+str (IllllIIllIIlIIIll )+'\n      ○ \x1b[31m0 = Check another Portal \x1b[0m\n      ○ \x1b[31mTo EXIT = PRESS ENTER \x1b[0m\n            \x1b[36mAɴsᴡᴇʀ =\x1b[31m ');print ('\x1b[90m')
+	if IIllIIlIIlllllIlI =='0':os .system (IIllIllIlIlIlIlll );IllIIllIIllIIIIII ()
+	else :exit ()
+if __name__ =='__main__':IllIIllIIllIIIIII ()
